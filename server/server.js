a6212fd6301c9e9983fde167aeda2cf3b27f5e01
chukwiz <chukswiz@gmail.com>
2020-02-21 23:05:36 +0100

    works

--- server/server.js
+++ server/server.js
@@ -26,10 +26,10 @@ app.use('/users',Users)
 
 if(process.env.NODE_ENV === "production"){
     app.disable('x-powered-by')
-    app.use(express.static('client/build'));
+    app.use(express.static('../build'));
 
     app.get('*',(req,res) => {
-        res.sendFile(path.resolve(__dirname, 'client','build','index.html'))
+        res.sendFile(path.resolve(__dirname, '../','build','index.html'))
     })
 }
 
